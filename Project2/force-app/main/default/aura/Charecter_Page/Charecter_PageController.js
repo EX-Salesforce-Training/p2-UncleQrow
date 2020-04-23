c5853c61417ae0b56d8d8e87c369fc3ca234866e
UncleQrow <matthe.trovillo@wsu.edu>
2020-04-23 14:29:39 -0400

    Work on components

--- Project2/force-app/main/default/aura/Charecter_Page/Charecter_PageController.js
+++ Project2/force-app/main/default/aura/Charecter_Page/Charecter_PageController.js
@@ -1,6 +1,6 @@
 ({
 	doInit : function(component, event, helper) {
-		var userId = $A.get("$SObjectType.CurrentUser.Id");
-        helper.grabProfile(component,event,helper,userId);
+        helper.grabProfile(component,event,helper);
+        helper.grabClasses(component,event,helper);
 	}
 })
--- Project2/force-app/main/default/aura/Charecter_Page/Charecter_PageHelper.js
+++ Project2/force-app/main/default/aura/Charecter_Page/Charecter_PageHelper.js
@@ -1,5 +1,50 @@
 ({
 	grabProfile : function() {
-		
-	}
+		var action = component.get("c.retrieveCharInfo");
+        action.setParams({});
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state == "SUCCESS"){
+                var CharInfo = response.getReturnValue();
+                component.set("v.Avatar", BaseCharInfo.get("Avatar"));
+                component.set("v.Server", BaseCharInfo.get("Server"));
+                component.set("v.CId", BaseCharInfo.get("charecterId"));
+            }
+            else if (state === "INCOMPLETE") {
+                console.log("Failed to connect Salesforce!!");
+            }
+                else if (state === "ERROR") {
+                    var errors = response.getError();
+                    if (!errors) {
+                        errors = [{"message" : "Unknown Error"}];
+                    }
+                    console.log(errors);
+                }
+        });
+        $A.enqueueAction(action);
+	},
+    grabClasses : function(){
+        var action = component.get("c.getClassInfo");
+        action.setParams({});
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state == "SUCCESS"){
+                var ClassInfo = response.getReturnValue();
+                let Classes = [...ClassInfo.keys()];
+                component.set("v.ClassNames", Classes);
+                component.set("v.ClassLevels", ClassInfo);
+            }
+            else if (state === "INCOMPLETE") {
+                console.log("Failed to connect Salesforce!!");
+            }
+                else if (state === "ERROR") {
+                    var errors = response.getError();
+                    if (!errors) {
+                        errors = [{"message" : "Unknown Error"}];
+                    }
+                    console.log(errors);
+                }
+        });
+        $A.enqueueAction(action);
+    }
 })
