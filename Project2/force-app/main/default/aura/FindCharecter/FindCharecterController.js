cdfc674487eb757e577f1e4bf836686a9a9b4f23
UncleQrow <matthe.trovillo@wsu.edu>
2020-04-23 17:04:45 -0400

    Update Fixes
    Fix COntroller to deal with duplicate charecters for Users

--- Project2/force-app/main/default/aura/FindCharecter/FindCharecterController.js
+++ Project2/force-app/main/default/aura/FindCharecter/FindCharecterController.js
@@ -1,8 +1,10 @@
 ({
-	handleClick : function (cmp, event, helper) {
-         helper.doSearch(cmp,event,helper);
+    handleClick : function (cmp, event, helper) {
+        helper.doSearch(cmp,event,helper);
+        component.set("v.Search", false);
     },
     SaveCharecter: function (cmp, event, helper){
         helper.saveId(cmp,event,helper);
+        component.set("v.Search", true);
     }
 })
